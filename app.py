cdad364be0d1e4a318f91298d64f555c57a1f6b6
amattheisen <andrew.ctr.mattheisen@faa.gov>
2019-03-21 07:40:27 +0000

    add filter dropdowns

--- app.py
+++ app.py
@@ -13,14 +13,18 @@ import pandas as pd
 # Custom Imports
 import parse_data
 
-
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+## Uncomment the following to use external stylesheets instead of the ones
+## included in this repository
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app = dash.Dash(__name__)
 
+# Generate pandas dataframe from data.txt
 dfs = parse_data.parse_data()
 
+
+# FUNCTIONS
 def generate_table(dataframe, max_rows=10):
     """
     Generate a table from a pandas dataframe.
@@ -38,7 +42,14 @@ def generate_table(dataframe, max_rows=10):
         ]) for i in range(min(len(dataframe), max_rows))]
     )
 
-# HEADER
+
+def get_data():
+    dfs = parse_data.parse_data()
+    return dfs
+
+
+# CONTENT
+## HEADER
 header_children = [
     html.H1(children='SOG Tools'),
     html.Hr(),
@@ -49,38 +60,39 @@ header_children = [
     ),
 ]
 
-# FILTERED DATA
+## FILTERED DATA
 filtered_children = [
     html.H2(children='Filtered Data'),
-    html.Div([
-        html.H3(children='Select Section'),
-        dcc.Dropdown(
-            id='section-dropdown',
-            options=[{'label': k, 'value': k} for k in dfs.keys()],
-            value='TOOLS',
-        ),
-    ], style={'width': '33%', 'display': 'inline-block'}),
-    html.Div([
-    ], style={'width': '.5%', 'display': 'inline-block'}),
-    html.Div([
-        html.H3(children='Select SubSection'),
-        dcc.Dropdown(id='subsection-dropdown'),
-    ], style={'width': '33%', 'display': 'inline-block'}),
-    html.Div([
-        html.H3(children='Select Item'),
-        dcc.Dropdown(id='items-dropdown'),
-    ], style={'width': '33%', 'float': 'right', 'display': 'inline-block'}),
-    html.Div(id='display-selected-values'),
+    html.Div(children=[
+        html.Div([
+            html.H3(children='Select Section'),
+            dcc.Dropdown(
+                id='section-dropdown',
+                options=[{'label': k, 'value': k} for k in dfs.keys()],
+                value='TOOLS',
+            ),
+        ], className="three columns"),
+        html.Div([
+            html.H3(children='Select SubSection'),
+            dcc.Dropdown(id='subsection-dropdown'),
+        ], className="three columns"),
+        html.Div([
+            html.H3(children='Select Item'),
+            dcc.Dropdown(id='items-dropdown'),
+        ], className="three columns"),
+    ], className="row"),
+    html.Div(id='display-selected-values', className="centered row"),
     html.Hr(),
 ]
 
-# ALL DATA
+## ALL DATA
 unfiltered_children = [
-    html.H2(children='All Data'),
+    html.H2(children='All Data', className="row"),
 ]
 for section in dfs:
-    unfiltered_children.append(html.H4(children=section))
+    unfiltered_children.append(html.H4(children=section, className="row"))
     unfiltered_children.append(generate_table(dfs[section]))
+
 
 # LAYOUT
 app.layout = html.Div([
@@ -94,6 +106,10 @@ app.layout = html.Div([
     Output('subsection-dropdown', 'options'),
     [Input('section-dropdown', 'value')])
 def set_section_options(selected_section):
+    """
+    Filter subsections based on section selection.
+
+    """
     return [{'label': i, 'value': i} for i in dfs[selected_section][1:]]
 
 
@@ -102,6 +118,10 @@ def set_section_options(selected_section):
     [Input('section-dropdown', 'value'),
      Input('subsection-dropdown', 'value')])
 def set_items_options(selected_section, selected_subsection):
+    """
+    Filter items based on section and subsection selections.
+
+    """
     return [{'label': i, 'value': i}
             for i in dfs[selected_section][selected_subsection].unique()]
 
@@ -110,6 +130,10 @@ def set_items_options(selected_section, selected_subsection):
     Output('subsection-dropdown', 'value'),
     [Input('subsection-dropdown', 'options')])
 def set_subsection_options(available_options):
+    """
+    Populate default selected subsection.
+
+    """
     return available_options[0]['value']
 
 
@@ -117,6 +141,10 @@ def set_subsection_options(available_options):
     Output('items-dropdown', 'value'),
     [Input('items-dropdown', 'options')])
 def set_item_options(available_options):
+    """
+    Populate default selected item.
+
+    """
     return available_options[0]['value']
 
 
@@ -126,6 +154,10 @@ def set_item_options(available_options):
      Input('section-dropdown', 'value'),
      Input('subsection-dropdown', 'value')])
 def set_display_children(selected_item, selected_section, selected_subsection):
+    """
+    Build table of filtered data based on selected section, subsection, and item.
+
+    """
     details = dfs[selected_section][dfs[selected_section][selected_subsection] == selected_item]
     return generate_table(details)
 
